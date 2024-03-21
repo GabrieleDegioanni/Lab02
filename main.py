b4edfdca28b74acbe6dd879bcf6d796a3957910e
GabrieleDegioanni <gabriele.degioanni@gmail.com>
2024-03-21 15:57:36 +0100

    Prima versione

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import translator as tr
 
+
+def stringaTraduzioni(lista):
+    stringa = ""
+    for i in range(len(lista)):
+        stringa += lista[i]
+        if i != len(lista) - 1:
+            stringa += ", "
+    return stringa
+
+
 t = tr.Translator()
 
+flag = True
 
-while(True):
+while flag:
 
     t.printMenu()
 
-    t.loadDictionary("filename.txt")
+    t.loadDictionary("dictionary.txt")
 
     txtIn = input()
 
     # Add input control here!
 
     if int(txtIn) == 1:
-        print()
-        txtIn = input()
-        pass
+        t.handleAdd(input("Ok, quale parola devo aggiungere?\n"))
     if int(txtIn) == 2:
-        pass
+        result = t.handleTranslate(input("Ok, quale parola devo cercare?\n"))
+        print(stringaTraduzioni(result))
     if int(txtIn) == 3:
-        pass
+        result = t.handleWildCard(input("Ok, quale parola devo cercare?\n"))
+        print(stringaTraduzioni(result))
     if int(txtIn) == 4:
-        break
+        for e in t.dizionario:
+            print(e + " " + stringaTraduzioni(t.dizionario[e]))
+    if int(txtIn) == 5:
+        flag = False
--- translator.py
+++ translator.py
@@ -1,27 +1,55 @@
 class Translator:
 
     def __init__(self):
-        pass
+        self.dizionario = {}
 
     def printMenu(self):
         # 1. Aggiungi nuova parola
         # 2. Cerca una traduzione
         # 3. Cerca con wildcard
         # 4. Exit
-        pass
+        print("------------------------------\nTranslator Alien-Italian\n------------------------------\n1. Aggiungi una nuova parola\n2. Cerca una traduzione\n3. Cerca con wildcard\n4. Stampa tutto il dizionario\n5. Exit\n------------------------------\n")
 
     def loadDictionary(self, dict):
         # dict is a string with the filename of the dictionary
-        pass
+        f = open(dict, "r").read().splitlines()
+        for i in range(0, len(f)):
+            s = f[i].split(" ")
+            self.dizionario[s[0].lower()] = [s[1].lower()]
 
     def handleAdd(self, entry):
         # entry is a tuple <parola_aliena> <traduzione1 traduzione2 ...>
-        pass
+        s = entry.split(" ")
+        if self.dizionario.get(s[0].lower()) is None:
+            self.dizionario[s[0].lower()] = []
+        for i in range(1, len(s)):
+            self.dizionario.get(s[0].lower()).append(s[i])
 
     def handleTranslate(self, query):
         # query is a string <parola_aliena>
-        pass
+        result = self.dizionario.get(query.lower())
+        return result
 
-    def handleWildCard(self,query):
+    def handleWildCard(self, query):
         # query is a string with a ? --> <par?la_aliena>
-        pass
+        i = 0
+        for j in range(len(query)):
+            if query[j] == "?":
+                i = j
+                break
+        listaParole = []
+        lettere = "abcdefghijklmnopqrstuvwxyz"
+        for j in range(len(lettere)):
+            parola = ""
+            for k in range(len(query)):
+                if k == i:
+                    parola += lettere[j]
+                else:
+                    parola += query[k]
+            listaParole.append(parola)
+        result = []
+        for p in listaParole:
+            if self.handleTranslate(p) is not None:
+                result += self.handleTranslate(p)
+        return result
+
